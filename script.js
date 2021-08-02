317ce0b5f4c94ca1123c3e3e86c1619f36509a86
Stormuke <fen1x@yandex.ru>
2021-08-02 14:42:26 +0300

    add: new button and header adaptive

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const handleEditClick = () => {
-  document.querySelector(".form__area").classList.remove("form__area_hide")
+  document.querySelector(".form__area").classList.remove("form__area_hide");
 }
 
-document.querySelector(".footer__button").addEventListener("click", handleEditClick)
+document.querySelector(".footer__button").addEventListener("click", handleEditClick);
 
 const formEditClick = () => {
-  document.querySelector(".form__area").classList.add("form__area_hide")
+  document.querySelector(".form__area").classList.add("form__area_hide");
 }
 
-document.querySelector(".form__close").addEventListener("click", formEditClick)
+document.querySelector(".form__close").addEventListener("click", formEditClick);
+
+const headerEditClick = () => {
+  document.querySelector(".header__navigation").classList.remove("header__navigation_hide");
+}
+
+document.querySelector(".header__button").addEventListener("click", headerEditClick);
