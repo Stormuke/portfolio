87e703220f28e01744295b6af4f5c0b6408089eb
Stormuke <fen1x@yandex.ru>
2021-08-03 12:05:56 +0300

    add: transition for href and burger

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 const handleEditClick = () => {
   document.querySelector(".form__area").classList.remove("form__area_hide");
 }
-
 document.querySelector(".footer__button").addEventListener("click", handleEditClick);
 
 const formEditClick = () => {
   document.querySelector(".form__area").classList.add("form__area_hide");
 }
-
 document.querySelector(".form__close").addEventListener("click", formEditClick);
 
 const headerEditClick = () => {
   document.querySelector(".header__navigation").classList.toggle("header__navigation_hide");
   document.querySelector('.header__button').classList.toggle('header__button_active');
 }
-
 document.querySelector(".header__button").addEventListener("click", headerEditClick);
+
+const anchors = document.querySelectorAll('a[href*="#"]');
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+
+    const blockID = anchor.getAttribute('href').substr(1)
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
