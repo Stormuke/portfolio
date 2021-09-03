80e1c64cb42e2d9241f980834450513342642652
Stormuke <fen1x@yandex.ru>
2021-09-03 16:29:18 +0300

    fix: mobile adaptive, fix: feedback window adaptive

--- scripts/script.js
+++ scripts/script.js
@@ -77,7 +77,7 @@ initialFeedback.forEach((element) => {
 
 //smothing scroll for anchors
 handleAnchors.forEach((anchor) => {
-  anchor.addEventListener('click', function (evt) {
+  anchor.addEventListener('click', (evt) => {
     evt.preventDefault()
     const blockID = anchor.getAttribute('href').substr(1);
     document.getElementById(blockID).scrollIntoView({
